278aae68a5bd91c7787f0a057dfe8cf9d2da508d
joerow <joerow@gmail.com>
2017-03-12 22:47:23 +0800

    gah

--- reqssystem/urls.py
+++ reqssystem/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^addOrder/', include('addOrder.urls')),
+    url(r'^addOrder/', include('addOrder.urls')), 
 ]
