3b31755e1760b123da5ed06d9df56cee97f0ee19
joerow <joerow@Josephs-MacBook.local>
2017-03-30 17:38:31 +0800

    Added some files for Mozilla tutorial

--- addOrder/models.py
+++ addOrder/models.py
@@ -4,12 +4,22 @@ from django.db import models
 # Create your models here.
 
 class Order(models.Model):
-    equipment = models.CharField(max_length=500)
-    date = models.DateField(null=True)
-    tperiod = models.IntegerField
+
+    # Fields
+    equipment = models.CharField(max_length=500, help_text="Type the equipment name")
+    date = models.DateField(null=True, help_text="Types the date in format dd/mm")
+    tperiod = models.IntegerField(help_text="Enter the teaching period")
+
+    # Meta
+    class Meta:
+        ordering = ["equipment"]
+
+    # methods
 
 
 class Staff(models.Model):
+
+    # Fields
     name = models.CharField(max_length=500)
     institution = models.CharField(max_length=500)
 
